51224118a17e714107f7edd10b40be86f9da0dc1
shaikhhira <shaikhhira393@gmail.com>
2021-01-09 16:30:24 +0500

    rating section add

--- app/src/main/java/com/example/carexpertsystem/HelperClasses/Constants.java
+++ app/src/main/java/com/example/carexpertsystem/HelperClasses/Constants.java
@@ -2,7 +2,7 @@ package com.example.carexpertsystem.HelperClasses;
 
 public class Constants {
 
-    private static final String ROOT_URL = "http://192.168.1.102/carproject/phpFolder/androdConn/";//chnage everytime by check IPconfig on cmd
+    private static final String ROOT_URL = "http://192.168.1.102/carExpertAndroidApp/phpFolder/androdConn/";//chnage everytime by check IPconfig on cmd
 
     public static final String URL_REGISTER = ROOT_URL + "registerUser.php";
     public static final String URL_LOGIN = ROOT_URL + "loginUser.php";
@@ -13,5 +13,8 @@ public class Constants {
     public static final String URL_ISSUE = ROOT_URL + "fetchIssue.php?MODELNAME=";
     public static final String URL_ISSUE_ISSUENAME ="&ISSUENAME=";
 
-    public static final String URL_SOLUTION=ROOT_URL+"fetchSolution.php?ISSUENAME=";
+    public static final String URL_SOLUTION = ROOT_URL + "fetchSolution.php?MODELNAME=";
+
+    public static final String URL_RATING=ROOT_URL+"ratingStar.php";
+
 }
